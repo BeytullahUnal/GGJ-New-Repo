1d4040867d36a3604e60a5f8e00dd0c027852a50
KordenizTANIL <kordeniz.tanil@gmail.com>
2017-01-22 11:28:47 +0300

    Hoydaaa

--- GGJ Game/Assets/Kordeniz/PlayerHealth.cs
+++ GGJ Game/Assets/Kordeniz/PlayerHealth.cs
@@ -1,12 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour {
 
 	public int maxhealth = 100;
 	public int currenthealth;
-
+	public Image HP1;
+	public Image HP2;
+	public Image HP3;
 
 	// Use this for initialization
 	void Awake ()
@@ -17,13 +20,13 @@ public class PlayerHealth : MonoBehaviour {
 	// Update is called once per frame
 	void Update () 
 	{
-		
+		 HealthHandler();
 	}
 
 	public void TakeDamage(int amount)
 	{
 		currenthealth -= amount;
-		//hpbar;
+
 		if (currenthealth <= 0) 
 		{
 			Dead ();
@@ -33,11 +36,38 @@ public class PlayerHealth : MonoBehaviour {
 	public void HealPlayer(int amount)
 	{
 		currenthealth += amount;
-		//hpbar;
+
 		if (currenthealth > maxhealth) 
 		{
 			currenthealth = maxhealth;
 		}
+	}
+
+	void HealthHandler()
+	{
+		if (currenthealth <= 75 && currenthealth >= 40) 
+		{
+			HP1.color = new Color32(255, 255, 255, 0);
+			HP2.color = new Color32(255, 255, 255, 255);
+			HP3.color = new Color32(255, 255, 255, 0);
+			Debug.Log ("AĞAĞAĞAA");
+		}
+
+		if (currenthealth < 40) 
+		{
+			HP1.color = new Color32(255, 255, 255, 0);
+			HP2.color = new Color32(255, 255, 255, 0);
+			HP3.color = new Color32(255, 255, 255, 255);
+		} 
+
+		if(currenthealth>75) 
+		{
+			HP1.color = new Color32 (255, 255, 255, 255);
+			HP2.color = new Color32(255, 255, 255, 0);
+			HP3.color = new Color32(255, 255, 255, 0);
+		}
+
+
 	}
 
 	void Dead()
